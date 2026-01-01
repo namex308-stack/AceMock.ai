2988dd8c03d5dd93559cbf3b138001cadab048dc
ali hashem <namex308@gmail.com>
2026-01-01 15:01:24 +0200

    Refactor next.config.js to remove echo commands

--- next.config.js
+++ next.config.js
@@ -1,23 +1,21 @@
-(
-echo const SITE = require('./src/config.js').SITE;
-echo.
-echo /** @type {import('next').NextConfig} */
-echo const nextConfig = {
-echo   reactStrictMode: true,
-echo   trailingSlash: SITE.trailingSlash,
-echo   basePath: SITE.basePathname !== '/' ? SITE.basePathname : '',
-echo   swcMinify: true,
-echo   poweredByHeader: false,
-echo.
-echo   output: 'standalone',
-echo.
-echo   images: {
-echo     remotePatterns: [
-echo       { protocol: 'https', hostname: 'images.unsplash.com' },
-echo       { protocol: 'https', hostname: 'source.unsplash.com' }
-echo     ]
-echo   }
-echo };
-echo.
-echo module.exports = nextConfig;
-) > next.config.js
+const SITE = require('./src/config.js').SITE;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: SITE.trailingSlash,
+  basePath: SITE.basePathname !== '/' ? SITE.basePathname : '',
+  swcMinify: true,
+  poweredByHeader: false,
+
+  output: 'standalone',  // مهم للديناميكية
+
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'source.unsplash.com' }
+    ]
+  },
+};
+
+module.exports = nextConfig;
